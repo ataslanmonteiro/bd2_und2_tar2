61faa067be8e823c34efeccc89df1f842eaaaad5
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2025-03-23 18:30:52 -0300

    teste 3

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
       username: 'root',
       password: '1234',
-      database: 'bd2_ud2_tar2',
+      database: 'bd2_und2_tar2',
       host: '127.0.0.1',
       dialect: 'mysql',
     },
