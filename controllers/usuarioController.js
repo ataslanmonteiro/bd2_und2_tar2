89c3008a3ee5077f48b8e7ae54075a715b64f502
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2025-03-23 18:28:34 -0300

    teste 2

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,7 +1,60 @@
 const { Usuario } = require('../models');
 
-exports.criarUsuario = async (req, res) => {...};
-exports.listarUsuarios = async (req, res) => {...};
-exports.atualizarUsuario = async (req, res) => {...};
-exports.deletarUsuario = async (req, res) => {...};
-exports.buscarUsuarioPorId = async (req, res) => {...};
+exports.criarUsuario = async (req, res) => {
+  try {
+    const usuario = await Usuario.create(req.body);
+    res.status(201).json(usuario);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.listarUsuarios = async (req, res) => {
+  try {
+    const usuarios = await Usuario.findAll();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.atualizarUsuario = async (req, res) => {
+  try {
+    const usuario = await Usuario.findByPk(req.params.id);
+    if (usuario) {
+      await usuario.update(req.body);
+      res.json(usuario);
+    } else {
+      res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.deletarUsuario = async (req, res) => {
+  try {
+    const usuario = await Usuario.findByPk(req.params.id);
+    if (usuario) {
+      await usuario.destroy();
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.buscarUsuarioPorId = async (req, res) => {
+  try {
+    const usuario = await Usuario.findByPk(req.params.id);
+    if (usuario) {
+      res.json(usuario);
+    } else {
+      res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
